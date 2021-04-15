7bfbf83c059345e2088ba98e647525cc3a8a8e03
majki97 <mwyszowski@gmail.com>
2021-04-15 20:55:48 +0100

    settings update

--- src/justchat/settings.py
+++ src/justchat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-+^g6bx!8d(ar-b8a45ngrz8e*yr-uv%9a1&*!nw_-s22$ibby'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party apps
     'channels',
     'chat',
 ]
